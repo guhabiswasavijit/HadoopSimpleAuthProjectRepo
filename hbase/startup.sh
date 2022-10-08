62c508b180d524b24cd2719bbd7724a2731d0518
unknown <guhabiswas.avijit@gmail.com>
2022-10-08 11:23:12 +0530

    hbase server

--- hbase/startup.sh
+++ hbase/startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo service ssh start\
+&& hadoop dfsadmin -safemode leave\
+&& source hbase-env.sh\
+&& hbase-daemon.sh start master\
 && hbase thrift start -p 9090 --infoport 9094\
 && hbase rest start -p 8082 --infoport 8081
+
